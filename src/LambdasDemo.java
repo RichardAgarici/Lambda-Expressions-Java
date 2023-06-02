d097e2efdd32e8fede4882c6356ca3d978bf1e0f
RichardAgarici <laurentiu.agarici@gmail.com>
2023-06-02 12:37:53 +0200

    Composive Function

--- src/LambdasDemo.java
+++ src/LambdasDemo.java
@@ -2,10 +2,8 @@ import java.util.function.Function;
 
 public class LambdasDemo {
 
-    public static void show(){
-        Function<String, Integer> map = str -> str.length();
-        var length = map.apply("Sky");
-        System.out.println(length);
+    public static void show() {
+
     }
 
 }
